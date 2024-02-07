b08f19fc096b31b0c1bb00dd5ca47a3cff9acd1c
JaviOGDev <javierortgar145@gmail.com>
2024-02-07 23:10:04 +0100

    Load of products from database. Manual pagination while checking when there are no products, also the total number of pages.

--- src/app/(shop)/page.tsx
+++ src/app/(shop)/page.tsx
@@ -1,9 +1,27 @@
+import { getPaginatedProductsWithImages } from "@/actions";
 import { ProductGrid, Title } from "@/components";
-import { initialData } from "@/seed/seed";
+import { redirect } from "next/navigation";
+// import { initialData } from "@/seed/seed";
 
-const products = initialData.products;
+// const products = initialData.products;
 
-export default function Home() {
+interface Props {
+  searchParams: {
+    page?: string;
+  };
+}
+
+export default async function Home({ searchParams }: Props) {
+  const page = searchParams.page ? parseInt(searchParams.page) : 1;
+
+  const { products, currentPage, totalPages } = await getPaginatedProductsWithImages({ page });
+
+  console.log(currentPage, totalPages);
+
+  if (products.length === 0) {
+    redirect("/");
+  }
+
   return (
     <>
       <Title title="Shop" subtitle="All products" classname="mb-2" />
